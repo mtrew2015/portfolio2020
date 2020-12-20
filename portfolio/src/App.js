3bb9589443040e3569819f57fd9fe9674c5b1bf0
Michael Trew <40902786+mtrew2015@users.noreply.github.com>
2020-12-19 23:52:19 -0500

    adding fonts/new section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,23 @@
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello, I'm Michael</h1>
-      <h1>I'm a Web Developer</h1>
+    <div className="wrapper">
+      <div className="App">
+        <h1>Hello, I'm Michael</h1>
+        <h1>I'm a Web Developer</h1>
+      </div>
+      <div className="skillsSection">
+        <h1>Technologies</h1>
+        <ul>
+          <li>
+            JavaScript
+          </li>
+          <li>
+            Node Js
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
